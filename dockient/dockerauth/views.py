e1335a08ffd011360e75df2dd62ad8081ae3dd9d
Sripathi Krishnan <sripathi.krishnan@hashedin.com>
2019-10-03 10:39:57 +0530

    Barebones implementation of docker login ui

--- dockient/dockerauth/views.py
+++ dockient/dockerauth/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
 
-# Create your views here.
+from .models import AuthToken
+
+
+@login_required
+def homepage(request):
+    context = {"user": request.user}
+    if request.method == "POST":
+        login_command = AuthToken.objects.get_docker_login(request.user)
+        context["docker_login"] = login_command
+    return render(request, "home.html", context=context)
+
+
+# Intentionally disabled django @login_required
+# This method is called by nginx whenever docker push / pull command is issued
+# This method should return status = 200 if the user is authorized to perform the action
+# Any other status code means nginx / docker registry should deny the action
+@require_http_methods(["POST"])
+def docker_registry_authenticate(request):
+    pass
